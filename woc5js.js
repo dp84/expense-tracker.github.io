33fc1cb2b75a3c125a50411253109bad2b1f2d7a
dp84 <122209357+dp84@users.noreply.github.com>
2023-12-27 23:33:42 +0530

    updated code files

--- woc5js.js
+++ woc5js.js
@@ -1,4 +1,5 @@
 var rowcnt = 0;
+
 function addrow() {
   rowcnt += 1;
   var table = document.getElementById("myTable");
@@ -6,19 +7,27 @@ function addrow() {
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  
+   cell1.innerHTML = prompt("Enter Description of Expense");
+   cell2.innerHTML = prompt("Enter Date of Expense in DD-MM-YYYY format");
+   cell3.innerHTML = prompt("Enter Amount Spent");
 
-  cell1.innerHTML = prompt("Enter Description of Expense");
-  cell2.innerHTML = prompt("Enter Date of Expense");
-  cell3.innerHTML = prompt("Enter Amount Spent");
+   let button = document.createElement('button');
+        button.innerText = 'Delete';  
+        button.setAttribute('onclick', 'delrow(this)');
+        button.setAttribute("style","background-color:black;text-align: center;color: white;margin-left: auto;margin-right: auto;margin-bottom:10px;width:100px");
+        cell4.appendChild(button);
+
   totalexp();
 }
 
-function delrow() {
-  var table = document.getElementById("myTable");
-  if (rowcnt != 0) {
-    var row = table.deleteRow(rowcnt);
+function delrow(r) {
+
+   var table = document.getElementById("myTable");
+   var i = r.parentNode.parentNode.rowIndex;
+   table.deleteRow(i);
     rowcnt -= 1;
-  }
   totalexp();
 }
 
@@ -28,5 +37,6 @@ function totalexp() {
   for (var i = 1; i <= rowcnt; i++) {
     tot += parseFloat(table.rows[i].cells[2].textContent);
   }
-  document.getElementById("tot").innerHTML = "Total Amount Spent " + tot;
+  document.getElementById("tot").innerHTML = "Total Money Spent Rs " + tot;
 }
+
